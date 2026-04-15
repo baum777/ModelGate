2e42ac1e18c2fef2e2dff0b363ca02a7aeb72445
t-wim <twim.baum@proton.me>
2026-04-15 22:41:36 +0200

    Fix nested Matrix route import

--- api/matrix/[...path].ts
+++ api/matrix/[...path].ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { handleVercelRequest } from "../../_handler.js";
+import { handleVercelRequest } from "../_handler.js";
 
 export default function matrixApiHandler(request: IncomingMessage, response: ServerResponse) {
   return handleVercelRequest(request, response);
