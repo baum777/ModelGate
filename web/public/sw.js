c2db34aefb2a01dc0c3eed6f4a0b7e300ac3d85e
t-wim <twim.baum@proton.me>
2026-04-28 18:08:43 +0200

    Integrate favicon asset bundle

--- web/public/sw.js
+++ web/public/sw.js
@@ -3,7 +3,15 @@ const SHELL_ASSETS = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
-  "/icon.svg"
+  "/icons/favicon.ico",
+  "/icons/favicon-16x16.png",
+  "/icons/favicon-32x32.png",
+  "/icons/favicon-48x48.png",
+  "/icons/apple-touch-icon.png",
+  "/icons/icon-64x64.png",
+  "/icons/icon-96x96.png",
+  "/icons/android-chrome-192x192.png",
+  "/icons/android-chrome-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
